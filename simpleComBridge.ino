abb88c7a9393ce40d41597aab98730d6a5948a71
Kim <kimachesita@gmail.com>
2014-08-28 19:59:49 -0700

    Updated to 2Way Mode

--- simpleComBridge.ino
+++ simpleComBridge.ino
@@ -1,6 +1,9 @@
 void setup(){
-   Serial.begin(9600);
-   Serial1.begin(9600);
+  Serial.begin(9600);
+  Serial1.begin(9600);
+  while(!Serial);
+  while(!Serial1);
+  //Serial.println("Simple Arduino Comm Bridge V1.0");
 }
 
 void loop(){
@@ -9,10 +12,15 @@ void loop(){
 void serialEvent(){
   if(Serial.available()){
     Serial1.write(Serial.read());
-  }if(Serial1.available()){
+  }
+}
+
+void serialEvent1(){
+  if(Serial1.available()){
     Serial.write(Serial1.read());
   }
 }
+
 
 
 
